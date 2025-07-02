9954e95fadd1447f31bab34e3e37fdfce31cf48a
down <37741552+Facefall@users.noreply.github.com>
2025-07-03 05:27:24 +0800

    fix: set_movement_target 随机点生成一致问题修复，change await physics_frame to process_frame

--- scene/characters/npc_states/walk_state.gd
+++ scene/characters/npc_states/walk_state.gd
@@ -15,17 +15,20 @@ func _ready() -> void:
 #	call_deferred 当前帧结束后再调用函数（空闲时间调用 
 	call_deferred('character_setup')
 
-	
 func character_setup() -> void:
-#	等待第一个物理帧结束
-	await get_tree().physics_frame
+#	fixed: physics_frame => process_frame
+#	等待第一个物理帧结束 
+#Godot 内部的随机数生成器可能在游戏启动时，在物理帧开始时具有一个相对固定的初始状态。如果你在第一个物理帧立即调用 map_get_random_point，它每次都可能从相同的初始状态开始生成随机数，导致结果一致
+#当你 await get_tree().process_frame 时，你等待的是一个渲染帧的结束。在一个渲染帧期间，游戏会处理各种事件、更新逻辑、播放动画等。这期间，很可能已经有足够的内部状态变化（包括随机数生成器的状态），使得在你调用 map_get_random_point 时，它能生成不同的随机点
+	await get_tree().process_frame
 	
 	set_movement_target()
 	
 func set_movement_target() -> void:
 	var map_id = navigation_agent_2D.get_navigation_map()
 	var map_layer = navigation_agent_2D.navigation_layers
-	var target_position: Vector2 = NavigationServer2D.map_get_random_point(map_id,map_layer, false)
+	
+	var target_position: Vector2 = NavigationServer2D.map_get_random_point(map_id, map_layer, false)
 	navigation_agent_2D.target_position = target_position
 	
 	speed = randf_range(min_speed, max_speed)
